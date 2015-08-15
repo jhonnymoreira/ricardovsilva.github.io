b93e108e472c9eafa736e54d502aa94d6222040d
Jhonny Moreira <xDeniedAccess@gmail.com>
2015-08-15 02:54:58 -0300

    Requires for Gulp in gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var plumber = require('gulp-plumber');
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
+var jeet = require('jeet');
+var cp = require('child_process');
